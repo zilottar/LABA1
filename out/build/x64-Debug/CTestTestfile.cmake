1349bc9588bded607571fcfc04c55d7bdb12a654
Кирилл <zoolohol@mail.ru>
2019-12-03 19:07:35 +0300

    lab2

---
+++ out/build/x64-Debug/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/kir/Desktop/LABA2
+# Build directory: C:/Users/kir/Desktop/LABA2/out/build/x64-Debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(unit_tests "C:/Users/kir/Desktop/LABA2/out/build/x64-Debug/tests.exe")
+set_tests_properties(unit_tests PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/kir/Desktop/LABA2/CMakeLists.txt;58;add_test;C:/Users/kir/Desktop/LABA2/CMakeLists.txt;0;")
